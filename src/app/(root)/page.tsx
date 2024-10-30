baa9784bc3232bc4d3deb5f09900a2bf64fb9e36
Bernard065 <bebenibernard@gmail.com>
2024-10-30 16:18:49 +0300

    Implement the search form

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,4 +1,13 @@
-export default function Home() {
+import SearchForm from "@/components/forms/SearchForm";
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query;
+  const params = { search: query || null };
+
   return (
     <>
       <section className="pink_container">
@@ -10,6 +19,8 @@ export default function Home() {
         <p className="sub-heading max-w-3xl">
           Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions
         </p>
+
+        <SearchForm query={query} />
       </section>
     </>
   );
