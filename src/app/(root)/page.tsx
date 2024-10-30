00e1e2b51c1de1c1312732fda8f9d252e52db47e
Bernard065 <bebenibernard@gmail.com>
2024-10-30 13:20:48 +0300

    implementing authentication with next-auth

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -3,7 +3,7 @@
 export default function Home() {
   return (
     <div className="">
-      Bernard
+      Bernard Bebeni
     </div>
   );
 }
--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -1,7 +1,71 @@
+import { auth, signIn, signOut } from "@/auth";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import { Button } from "../ui/button";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { BadgePlus, LogOut } from "lucide-react";
 
-const Navbar = () => {
-  return <div>Navbar</div>;
+const Navbar = async () => {
+  const session = await auth();
+
+  if (!session?.user) return null;
+
+  return (
+    <header className="px-5 py-3 bg-white shadow-sm">
+      <nav className="flex justify-between items-center">
+        <Link href="/">
+          <Image
+            src="/assets/images/logo.png"
+            width={144}
+            height={30}
+            alt="logo"
+          />
+        </Link>
+
+        <div className="flex items-center gap-5">
+          {session && session?.user ? (
+            <>
+              <Link href="/startup/create">
+                <span className="max-sm:hidden">Create</span>
+                <BadgePlus className="size-6 sm:hidden" />
+              </Link>
+              <form
+                action={async () => {
+                  "use server";
+                  await signOut();
+                }}
+              >
+                <Button className="max-sm:hidden" type="submit">
+                  <span>Sign Out</span>
+                </Button>
+                <LogOut className="size-6 sm:hidden text-red-500" />
+              </form>
+
+              <Link href={`/user/${session?.id}`}>
+                <Avatar className="size-10">
+                  <AvatarImage
+                    src={session?.user?.image || ""}
+                    alt={session?.user?.name || ""}
+                  />
+                  <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+              </Link>
+            </>
+          ) : (
+            <form
+              action={async () => {
+                "use server";
+                await signIn("github");
+              }}
+            >
+              <Button type="submit">Sign In</Button>
+            </form>
+          )}
+        </div>
+      </nav>
+    </header>
+  );
 };
 
 export default Navbar;
