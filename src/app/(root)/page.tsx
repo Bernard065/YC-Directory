6acf0e841555676feccf2faef71c625f604a6753
Bernard065 <bebenibernard@gmail.com>
2024-10-31 03:46:03 +0300

    Implementing the startup card for startups

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,3 +1,4 @@
+import StartupCard from "@/components/cards/StartupCard";
 import SearchForm from "@/components/forms/SearchForm";
 
 export default async function Home({
@@ -6,7 +7,25 @@ export default async function Home({
   searchParams: Promise<{ query?: string }>;
 }) {
   const query = (await searchParams).query;
-  const params = { search: query || null };
+
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 55,
+      author: {
+        _id: 1,
+        name: "Bernard",
+        image:
+          "https://images.unsplash.com/photo-1502823403499-6ccfcf4fb453?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D",
+      },
+      _id: 1,
+      description: "This is a description.",
+      image:
+        "https://images.unsplash.com/photo-1516381093400-a0fecb601de2?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cG9zdHN8ZW58MHx8MHx8fDA%3D",
+      category: "Robots",
+      title: "We Robots",
+    },
+  ];
 
   return (
     <>
@@ -21,6 +40,20 @@ export default async function Home({
         </p>
 
         <SearchForm query={query} />
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "All Startups"}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post) => <StartupCard key={post?._id} post={post} />)
+          ) : (
+            <p className="no-results">No Startups found</p>
+          )}
+        </ul>
       </section>
     </>
   );
